718145bff3c84fef3ceef47150fe276961cd24a7
mdratul4505 <dev.raul.coder@gmail.com>
2026-04-16 01:42:36 +0600

    create zapshift server repo

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require("cors");
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+// middle ware
+app.use(express.json())
+app.use(cors()); 
+
+app.get('/', (req, res) => {
+  res.send('Zap is shifting shifting!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
