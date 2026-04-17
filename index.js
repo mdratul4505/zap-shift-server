b866a2e69957cb7d2b058a7c15ae32cea3bb87cf
mdratul4505 <dev.raul.coder@gmail.com>
2026-04-18 00:58:45 +0600

    added payment related api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 require('dotenv').config()
 const { MongoClient, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 // middle ware
 app.use(express.json())
@@ -42,6 +43,14 @@ async function run() {
       res.send(result)
     })
 
+    // payment parcel api
+    app.get('/parcels/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await parcelCollection.findOne(query);
+      res.send(result )
+    })
+
     app.post('/parcels', async (req, res)=>{
       const parcel  = req.body;
       parcel.createAt = new Date();
@@ -59,6 +68,43 @@ async function run() {
 
 
 
+
+
+
+
+
+    // payment related apis
+    app.post('/create-checkout-session', async (req , res) =>{
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+
+      const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        price_data:{
+          currency:'USD',
+          unit_amount: amount,
+          product_data:{
+            name: paymentInfo.parcelName
+          }
+
+        },
+        
+        quantity: 1,
+      },
+    ],
+    customer_email: paymentInfo.SenderEmail,
+    mode: 'payment',
+    metadata:{
+      parcelId: paymentInfo.parcelId
+    },
+    success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-succes`,
+    cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+  })
+  console.log(session)
+  res.send({url:session.url})
+    })
 
 
     // Send a ping to confirm a successful connection
